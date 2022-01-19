c71734b865226e2b98eba29927a2cf92cb90531f
Jenneeee <jenne@cattoor.net>
2022-01-19 16:08:29 +0100

    Added the little accent before the paragraph #17

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,11 +1,13 @@
 <section class="content">
   <section class="header">
     <h1 class="title">Futura</h1>
-    <h2 class="title-small">The geometric sans-serif font</h2>
+    <h2 class="title-small">The geometric <br class="mobile-br">sans-serif font</h2>
   </section>
   <section class="introduction">
-    <p class="introduction-text">Futura was designed in 1927 by Paul Renner as a contribution on the <span class="underline">New Frankfurt-project</span>.
-      It’s based on <span class="underline">geometric shapes</span>, similar in spirit to the <span class="underline">Bauhaus</span> design style of the period.<p>
+    <p class="introduction-text"><img class="introduction-shapes" src="./assets/img/shapes.svg" alt="geometric shapes"><br class="mobile-br">
+      Futura was designed in 1927 by Paul Renner as a contribution on the <span class="underline">New Frankfurt-project</span>.
+      It’s based on <span class="underline">geometric shapes</span>, similar in spirit to the <span class="underline">Bauhaus</span> design style of the period.
+      <p>
   </section>
 
 </section>
