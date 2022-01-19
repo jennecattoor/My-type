97cf7966b05b4c31a719261df396f1ba7e89eb7b
Jenneeee <jenne@cattoor.net>
2022-01-19 21:01:42 +0100

    Started working on characteristics section #20

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,5 +1,7 @@
 <section class="content">
-  <div class="slanted"><p>slanted</p></div>
+  <div class="slanted">
+    <p>slanted</p>
+  </div>
   <section class="header">
     <h1 class="title">Futura</h1>
     <h2 class="title-small">The geometric <br class="mobile-br">sans-serif font</h2>
@@ -8,7 +10,25 @@
     <p class="introduction-text"><img class="introduction-shapes" src="./assets/img/shapes.svg" alt="geometric shapes"><br class="mobile-br">
       Futura was designed in 1927 by Paul Renner as a contribution on the <span class="underline">New Frankfurt-project</span>.
       It’s based on <span class="underline">geometric shapes</span>, similar in spirit to the <span class="underline">Bauhaus</span> design style of the period.
-      <p>
+    <p>
   </section>
-
+  <section class="characteristics">
+    <h3 class="section-title character-title">Characteristics of Futura</h3>
+    <div class="character_item">
+      <hr>
+      <p class="character_item-title"><img class="character-shapes" src="./assets/img/shapes2.svg" alt="geometric shapes">High ascenders & circular counterspace</p>
+      <hr>
+      <p class="character_item-description">The ascenders rise above the ascender line. This create their really iconic shape but makes the font harder to read on long texts.
+        As the letters are based on geometric shapes, they also have a circular counterspace.
+      </p>
+    </div>
+    <div class="character_item">
+      <hr>
+      <p class="character_item-title"><img class="character-shapes" src="./assets/img/shapes2.svg" alt="geometric shapes">Low crossbar & extended width</p>
+      <hr>
+      <p class="character_item-description">Lowercase letters have a low corssbar. This creates a bigger difference between capitals and and lowercase letters.
+        Not only is the crossbar lower, Capitals like the letter F have an extended width
+      </p>
+    </div>
+  </section>
 </section>
