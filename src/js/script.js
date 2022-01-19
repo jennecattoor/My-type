ef23e96b1f395cb99be8c7ea7cd9f788955df276
Jenneeee <jenne@cattoor.net>
2022-01-19 14:02:19 +0100

    Placed content of header in the right place #15

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 export const init = () => {
+  window.addEventListener('resize', () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
 };
--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -3,4 +3,9 @@
     <h1 class="title">Futura</h1>
     <h2 class="title-small">The geometric sans-serif font</h2>
   </section>
+  <section class="introduction">
+    <p>Futura was designed in 1927 by Paul Renner as a contribution on the New Frankfurt-project.
+      It’s based on geometric shapes, similar in spirit to the Bauhaus design style of the period.<p>
+  </section>
+
 </section>
