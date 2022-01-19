fe23e2f8b03aff3ed1e5fc3811f6a9e386362bd1
Jenneeee <jenne@cattoor.net>
2022-01-19 20:01:28 +0100

    Attempting to fix bug where screen resizes on IOS #26

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,8 @@
-const vh = window.innerHeight * 0.01;
-
 export const init = () => {
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
   window.addEventListener('resize', () => {
+    const vh = window.innerHeight * 0.01;
     if (window.innerWidth >= 1024) {
       document.documentElement.style.setProperty('--vh', `${vh}px`);
-      console.log('changed size');
     }
   });
-
 };
